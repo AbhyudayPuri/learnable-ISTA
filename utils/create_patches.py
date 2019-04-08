6251fad27787a46fd700c624ea6923e2caebb133
Abhyuday Puri <a.96puri@gmail.com>
2019-04-08 00:41:55 -0400

    Basic batch generation

--- utils/create_patches.py
+++ utils/create_patches.py
@@ -1,21 +1,34 @@
 from sklearn.feature_extraction import image
-import cv2 as cv
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import glob
+import numpy as np
+import cv2 as cv
 
-## Ppath of the folder that contains the data 
-path = '../data/train/*.jpg'
+def create_patches(path, lines, num_patches):
+	# path = the path of where the data is stored
+	# lines = a list that contains all the file names
+	# num_patches = the number of patches will be generated from each image
 
-## Reading all the files in the directory specified by the path
-files = glob.glob(path)   
+	# Total number of images
+	num_images = len(lines)
+	# Creating an array to store the patched data
+	output = np.zeros([num_images * num_patches, 10, 10, 3], dtype=np.uint8)
 
-## Reading the images in the given directory
-for file in files:
-	im = cv.imread(file)
-	## Generate patches for each of the image and store them
-	patches = image.extract_patches_2d(im, (10, 10))
-	[num_patches, _, _, _] = patches.shape
+	for i in range(num_images):
+		im = plt.imread(path + lines[i] + '.jpg')
+		output[i*num_patches : (i+1)*num_patches,:,:,:] = image.extract_patches_2d(im, (10, 10), max_patches=num_patches)
 
+	return output
 
-print(num_patches)
-# cv.imshow("image", patches[0])
-# cv.waitKey(0)
+path = '../data/train/'
+fp = open('../data/iids_train.txt')
+lines = fp.read().splitlines() # Create a list containing all lines
+fp.close()
+num_patches = 10
+
+output = create_patches(path, lines, num_patches)
+print(output[1].shape)
+plt.imshow(output[1])
+plt.show()
--- utils/generate_batch.py
+++ utils/generate_batch.py
@@ -1,6 +1,24 @@
 from sklearn.feature_extraction import image
 import cv2 as cv
 import glob
+from random import shuffle
 
-def generate_batch():
-	
+
+path = '../data/train/*.jpg'
+files = glob.glob(path)
+
+for file in files:
+	im = cv.imread(file)
+
+fp = open('../data/iids_train.txt')
+lines = fp.read().splitlines() # Create a list containing all lines
+fp.close()
+
+im = cv.imread('../data/train/' + str(lines[0]) + '.jpg')
+cv.imshow("image", im)
+cv.waitKey(0)
+print(lines)
+print(len(lines))
+
+shuffle(lines)
+print(lines)
