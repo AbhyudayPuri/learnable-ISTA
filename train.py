cec288b3228757b08e53b43a5d7e68e8c03f1652
Abhyuday Puri <a.96puri@gmail.com>
2019-05-10 14:56:38 -0400

    train

---
+++ train.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np 
+import torchvision
+from utils.create_patches import create_patches
+from lista.lista import lista
+from utils.fast_ista import fast_ista
+
+print('hello')
