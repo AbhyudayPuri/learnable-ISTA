6dc6baafa50f9abf6741f94b54edf3f8f294237c
Abhyuday Puri <a.96puri@gmail.com>
2019-05-15 16:17:09 -0400

    create dataset

---
+++ utils/preprocess_data.py
@@ -0,0 +1,31 @@
+from utilts.create_patches import create_patches
+from utils.fast_ista import fast_ista
+
+# Path to where the data is stored 
+path = 'data/train/'
+
+# Reading the text file that contains names of all the images
+fp = open('data/iids_train.txt')
+lines = fp.read().splitlines() # Create a list containing all lines
+fp.close()
+
+# Set the hyper-parameters
+num_patches = 10000
+alpha = 0.1
+
+# Read the dictionary
+Wd = np.load('Wd.npy')
+
+# Create the patches
+X = create_patches(path, lines, num_patches)
+
+# Save the data patches
+np.save('X_train.npy', X)
+
+# Create the sparse code
+Z[0:50000] = fast_ista(X[0:50000], Wd, alpha)
+Z[50000:100000] = fast_ista(X[50000:100000], Wd, alpha)
+Z[100000:150000] = fast_ista(X[100000:150000], Wd, alpha)
+Z[150000:200000] = fast_ista(X[150000:200000], Wd, alpha)
+
+np.save('Z_train.npy', Z)
