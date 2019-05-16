0b4cf1ccc7dc033eb3ed8a9c8d7afa92ac73bda1
Abhyuday Puri <a.96puri@gmail.com>
2019-05-16 00:30:27 -0400

    push

--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -8,7 +8,7 @@ alpha = 0.1
 
 # Read the input data
 X = np.load('/home/ecbm6040/learnable-ISTA/X_train.npy')
-X = X[:,1400000:2000000]
+X = X[:,800000:1400000]
 # Read the dictionary
 Wd = np.load('/home/ecbm6040/learnable-ISTA/Wd.npy')
 
@@ -20,14 +20,14 @@ print('Data Created')
 
 for i in range(60):
 	start = time.time()
-	print('Data Chunk {}/80'.format(i+1))
+	print('Data Chunk {}/60'.format(i+1))
 	Z[:, i*10000 : (i+1)*10000] = fast_ista(X[:, i*10000 : (i+1)*10000], Wd, alpha)
 	end = time.time()
-	np.save('/home/ecbm6040/learnable-ISTA/Z2_train.npy', Z)
+	np.save('/home/ecbm6040/learnable-ISTA/Z3_train.npy', Z)
 	print("Time taken for 1 mini-batch: {}".format(end-start))
 
 print('Labels Created')
 
 # Save the data
 # np.save('/home/ecbm6040/learnable-ISTA/X_train.npy', X)
-np.save('/home/ecbm6040/learnable-ISTA/Z2_train.npy', Z)
+np.save('/home/ecbm6040/learnable-ISTA/Z3_train.npy', Z)
