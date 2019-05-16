41426e6502a6c85830079ec96cd63391c1e3ad2a
Abhyuday Puri <a.96puri@gmail.com>
2019-05-16 14:12:40 -0400

    create labels

--- utils/create_labels.py
+++ utils/create_labels.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 Z1 = np.load('/home/ecbm6040/learnable-ISTA/Z1_train.npy')
+print(Z1.shape)
 Z2 = np.load('/home/ecbm6040/learnable-ISTA/Z_8_14.npy')
-Z1 = np.load('/home/ecbm6040/learnable-ISTA/Z_14_20.npy')
+print(Z2.shape)
+Z3 = np.load('/home/ecbm6040/learnable-ISTA/Z_14_20.npy')
+print(Z3.shape)
 
 Z = np.zeros((400,2000000))
 
