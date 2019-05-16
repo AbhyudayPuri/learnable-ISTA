a9182c98353c697749c919d62ecf8974174bdf13
Abhyuday Puri <a.96puri@gmail.com>
2019-05-16 19:29:52 -0400

    train

--- lista/lista.py
+++ lista/lista.py
@@ -16,7 +16,7 @@ class lista(nn.Module):
 		self.m = 400
 
 		# self.W = torch.nn.Parameter(torch.randn((self.m, self.n), requires_grad=True))
-		self.W = torch.nn.Parameter(torch.from_numpy(np.random.normal(0, 1/self.n, (self.n, self.m))), requires_grad=True)
+		self.W = torch.nn.Parameter(torch.from_numpy(np.random.normal(0, 1/self.m, (self.m, self.n))), requires_grad=True)
 		self.S = torch.nn.Parameter(torch.from_numpy(np.random.normal(0, 1/self.m, (self.m, self.m))), requires_grad=True)
 		# self.S = torch.nn.Parameter(torch.randn((self.m, self.m), requires_grad=True))
 		self.soft_thresh = nn.Softshrink(lambd=0.1)
